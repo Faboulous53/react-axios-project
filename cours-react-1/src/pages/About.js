44c09660e1362dc3d40d9fa8b08d8c986b82dcd4
Fabien Delpierre <fab.delpierre@gmail.com>
2023-01-24 15:02:22 +0100

    creat navigation

---
+++ cours-react-1/src/pages/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <h1>A propos</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+                repudiandae quis, aliquid nulla reiciendis repellat officia
+                maxime nemo vero eum, quisquam debitis facere voluptatem ab.
+                Nihil dolorum vel saepe deleniti debitis asperiores odio quidem,
+                non labore accusamus, maxime odit earum sequi temporibus?
+                Dignissimos aut dolore natus, facilis aliquam rem cum mollitia
+                cumque repudiandae quia id possimus sapiente temporibus magnam
+                nemo illum perferendis fugiat officia blanditiis perspiciatis
+                repellendus? Ipsa quasi commodi quaerat, blanditiis iure culpa
+                in facere laboriosam laudantium, reprehenderit odit esse velit
+                sunt adipisci asperiores consequuntur ex odio sint animi, soluta
+                voluptate provident corrupti aut aperiam. Vitae, officia? Nulla,
+                hic.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi quis, modi fugiat, reprehenderit quo quos incidunt
+                adipisci autem provident ullam velit soluta iste dolor repellat
+                commodi nemo quasi sunt perferendis! Doloribus beatae eligendi
+                illo aperiam possimus, eius soluta quidem? Iusto, delectus
+                repellat. Est ullam amet totam nulla? Laboriosam, obcaecati
+                inventore!
+            </p>
+        </div>
+    );
+};
+
+export default About;
