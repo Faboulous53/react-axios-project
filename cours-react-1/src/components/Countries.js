1f0bff9842b42aed5b5ef56695f523804320e999
Fabien Delpierre <fab.delpierre@gmail.com>
2023-01-25 15:18:50 +0100

    crate filter countries

--- cours-react-1/src/components/Countries.js
+++ cours-react-1/src/components/Countries.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
+import Card from './Card';
 
 const Countries = () => {
 
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
     // Le useEffect se joue lorsque le composant est monté
+    const [rangeValue, setRangeValue] = useState(36);
+    const radios = ["Africa","America", "Asia", "Europe", 'Oceania'];
 
     useEffect(() => {
         axios.get('https://restcountries.com/v3.1/all')
@@ -13,7 +16,23 @@ const Countries = () => {
 
     return (
         <div className='countries'>
-            <h1>Countries</h1>
+            <ul className="radio-container">
+                <input type="range" min="1" max="250" defaultValue={rangeValue} onChange={(e) => setRangeValue(e.target.value)}/>
+                {radios.map((continent) => (
+                    <li>
+                    <input type="radio" id={continent} name='continentRadio'/>
+                    <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
+                
+            </ul>
+            <ul>
+                {
+                    data
+                    .slice(0, rangeValue)
+                    .map((country, index) => <Card key={index}  country={country}/>)
+                }
+            </ul>
         </div>
     );
 };
